5b6057be18c61f3f9459217bb0ab2706cba50ef4
Daniel Sandberg <bjorndaniel@gmail.com>
2024-10-09 13:01:19 +0200

    Add Blazor projects and update UI components
    - Added new Blazor WebAssembly project `NanoTemp.UI.Client` with `Measurement` class, `Counter.razor` component, and necessary configurations.
    - Added new Blazor Server project `NanoTemp.UI` with `App.razor`, `Routes.razor`, `MainLayout.razor`, `NavMenu.razor`, `Error.razor`, `Home.razor`, `Weather.razor`, and `TempHub.cs` for SignalR.
    - Updated `GlobalUsings.cs` with global using directives for ASP.NET Core and project-specific namespaces.
    - Updated `Program.cs` files to initialize and configure Blazor WebAssembly and Server applications.
    - Added `appsettings.Development.json` and `appsettings.json` for configuration.
    - Updated `_Imports.razor` files to include necessary using directives.
    - Added CSS rules for styling Blazor components and a new `favicon.png`.
    - Updated `NanoTemp.sln` to include new projects and build configurations.
    - Minor formatting change in `Measurement.cs` in `NanoTemp.Data` namespace.

---
+++ NanoTemp.UI/NanoTemp.UI/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.ResponseCompression;
+global using Microsoft.AspNetCore.SignalR;
+global using NanoTemp.UI.Client;
+global using NanoTemp.UI.Components;
+global using NanoTemp.UI.Hubs;
---
+++ NanoTemp.UI/NanoTemp.UI/Hubs/TempHub.cs
@@ -0,0 +1,9 @@
+﻿namespace NanoTemp.UI.Hubs;
+
+public class TempHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
