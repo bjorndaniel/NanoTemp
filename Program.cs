994add20bf2154de0842b436ad6ceea3e5e2d31e
Daniel Sandberg <bjorndaniel@gmail.com>
2024-10-10 11:00:58 +0200

    WIP

--- Program.cs
+++ Program.cs
@@ -1,26 +1,26 @@
-using Iot.Device.Ssd13xx;
 using nanoFramework.Device.Bluetooth;
 using nanoFramework.Device.Bluetooth.Advertisement;
-using nanoFramework.Hardware.Esp32;
+using nanoFramework.Networking;
 using NanoTemp.Data;
 using System;
-using System.Device.I2c;
 using System.Diagnostics;
+using System.Net.Http;
 using System.Threading;
 
 namespace NanoTemp
 {
     public class Program
     {
-        private static Ssd1306 _screen;
         private static BluetoothLEAdvertisementWatcher _watcher;
+        const string ssid = "Daniel 15Pro";
+        const string password = "T3stP4ssw0rd!!";
+        private static HttpClient _httpClient;
+        private static Measurement _message;
 
         public static void Main()
         {
             Debug.WriteLine("Hello from nanoFramework!");
-            InitializeSSD1306();
-            _screen.DrawString(2, 2, "Hello", 2, center: true);
-            _screen.Display();
+            InitializeWifi();
             InitializeBluetooth();
             _watcher.Start();
             while (true)
@@ -30,19 +30,29 @@ namespace NanoTemp
                 _watcher.Start();
             }
 
-            void InitializeSSD1306()
-            {
-                Configuration.SetPinFunction(17, DeviceFunction.I2C1_DATA);
-                Configuration.SetPinFunction(18, DeviceFunction.I2C1_CLOCK);
-                _screen = new Ssd1306(I2cDevice.Create(new I2cConnectionSettings(1, Ssd1306.DefaultI2cAddress)), Ssd13xx.DisplayResolution.OLED128x64);
-                _screen.ClearScreen();
-                _screen.Font = new BasicFont();
-            }
-
             void InitializeBluetooth()
             {
                 _watcher = new();
                 _watcher.Received += Watcher_Received;
+            }
+
+            void InitializeWifi()
+            {
+                var cs = new CancellationTokenSource(20_000);
+                var success = WifiNetworkHelper.ConnectDhcp(ssid, password, requiresDateTime: true, token: cs.Token);
+                if (!success)
+                {
+                    Debug.WriteLine($"Cannot connect to the WiFi");
+                    //if (WifiNetworkHelper.HelperException != null)
+                    //{
+                    //    Debug.WriteLine($"ex: {WifiNetworkHelper.HelperException}");
+                    //}
+                }
+                else
+                {
+                    Debug.WriteLine("Connected successfully");
+                    _httpClient = new HttpClient();
+                }
             }
 
             static void Watcher_Received(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
@@ -61,26 +71,26 @@ namespace NanoTemp
                         var tm = BitConverter.ToString(b);
                         if (tm == "04-99") //https://www.bluetooth.com/wp-content/uploads/Files/Specification/HTML/Assigned_Numbers/out/en/Assigned_Numbers.pdf?v=1728219678943
                         {
-                            var message = RuuviDf5Decoder.DecodeMessage(bytes);
-                            if (message is not null)
+                            _message = RuuviDf5Decoder.DecodeMessage(bytes);
+                            if (_message is not null)
                             {
-                                _screen.ClearScreen();
-                                _screen.DrawString(2, 2, message.TemperatureDisplay, 1, true);
-                                _screen.DrawString(2, 18, message.HumidityDisplay, 1, true);
-                                _screen.DrawString(2, 34, message.BatteryDisplay, 1, true);
-                                _screen.DrawString(2, 50, message.MacDisplay, 1, true);
-                                _screen.Display();
-                                Debug.WriteLine($"Temperature: {message.TemperatureDisplay}");
-                                Debug.WriteLine($"Humidity: {message.HumidityDisplay}");
-                                Debug.WriteLine($"Battery: {message.BatteryDisplay}");
-                                Debug.WriteLine($"Mac Address: {message.MacAddress}");
-                                Debug.WriteLine($"Time: {message.TimeStamp}");
+                                //_httpClient ??= new HttpClient();
+                                //var requestUri = "http://5058/measure";
+                                //var content = new StringContent(JsonConvert.SerializeObject(message));
+                                //var response = _httpClient.Post(requestUri, content);
+                                //var mess = response.Content.ReadAsString();
+                                //Debug.WriteLine(mess);
+                                Debug.WriteLine($"Temperature: {_message.TemperatureDisplay}");
+                                Debug.WriteLine($"Humidity: {_message.HumidityDisplay}");
+                                Debug.WriteLine($"Battery: {_message.BatteryDisplay}");
+                                Debug.WriteLine($"Mac Address: {_message.MacAddress}");
+                                Debug.WriteLine($"Time: {_message.TimeStamp}");
                                 Thread.Sleep(5000);
                             }
                         }
                         else
                         {
-                            Console.WriteLine(tm);
+                            Debug.WriteLine(tm);
                         }
                     }
                 }
